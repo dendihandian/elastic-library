9a8ee2fe8aa42fe8841d09db6f635a1ad127e31a
Dendi Handian <dhn@voxteneo.asia>
2020-04-06 01:26:57 +0700

    php artisan twill:module Book -B -T -S -M -F -P -R

---
+++ laravel/database/migrations/2020_04_05_182406_create_books_tables.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateBooksTables extends Migration
+{
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            // this will create an id, a "published" column, and soft delete and timestamps columns
+            createDefaultTableFields($table);
+            
+            $table->integer('position')->unsigned()->nullable();
+            
+            // add those 2 columns to enable publication timeframe fields (you can use publish_start_date only if you don't need to provide the ability to specify an end date)
+            // $table->timestamp('publish_start_date')->nullable();
+            // $table->timestamp('publish_end_date')->nullable();
+        });
+
+        Schema::create('book_translations', function (Blueprint $table) {
+            createDefaultTranslationsTableFields($table, 'book');
+            $table->string('title', 200)->nullable();
+            $table->text('description')->nullable();
+        });
+
+        Schema::create('book_slugs', function (Blueprint $table) {
+            createDefaultSlugsTableFields($table, 'book');
+        });
+
+        Schema::create('book_revisions', function (Blueprint $table) {
+            createDefaultRevisionsTableFields($table, 'book');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('book_revisions');
+        Schema::dropIfExists('book_translations');
+        Schema::dropIfExists('book_slugs');
+        Schema::dropIfExists('books');
+    }
+}
